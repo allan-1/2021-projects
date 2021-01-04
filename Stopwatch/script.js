bfc571829f160c8c92d783451af3c9b2b0ff3a6a
Allan Maina <Amuturi01@gmail.com>
2021-01-04 20:01:21 +0300

    stopwatch

---
+++ Stopwatch/script.js
@@ -0,0 +1,46 @@
+const minutes = document.querySelector('.minutes');
+const seconds = document.querySelector('.seconds');
+
+const start = document.querySelector('.start');
+const stop = document.querySelector('.stop')
+const reset = document.querySelector('.reset')
+
+let starttime = 0;
+let minutestime = 0;
+
+// event listeners
+start.addEventListener('click', startCounter);
+stop.addEventListener('click', stopCounter);
+reset.addEventListener('click', resetCounter);
+
+// functions
+
+function startCounters() {
+    document.querySelector('.start').disabled = true;
+    starttime ++
+    if (starttime >= 60) {
+        minutestime ++
+        starttime = 0
+    }
+    if (starttime <= 9) {
+        seconds.innerText = '0' + starttime + ' S'
+        minutes.innerText = minutestime + 'Min'
+    }
+    else {
+        seconds.innerText = starttime + 'S'
+        minutes.innerText = minutestime + 'Min'
+    }
+}
+function startCounter() {
+    timer = setInterval(startCounters, 1000)
+}
+
+function stopCounter() {
+    document.querySelector('.start').disabled = false;
+    clearInterval(timer)
+}
+function resetCounter() {
+    stopCounter()
+    seconds.innerText = '00'
+    minutes.innerText = '00'
+}
